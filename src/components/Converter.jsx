8e35119779c7cbfffe2c229d14d3a966b13b185b
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-10-17 14:32:20 +0300

    axios on working

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from 'react';
+import axios from 'axios';
 import Block from './Block';
 import ButtonReverse from './ButtonReverse';
 import './css/Converter.css';
@@ -10,6 +11,27 @@ export default function Converter() {
     const [resultValue, setResultValue] = useState('0');
 
     let info = `${inputValue} ${validCurrency1} == ${resultValue} ${validCurrency2}`;
+
+    async function fetchCurrency() {
+        try {
+            const response = await axios.get("https://www.cbr-xml-daily.ru/daily_json.js");
+
+            let value1;
+            let value2;
+
+            validCurrency1 === 'RUB' ? value1 = 1 : value1 = response.data.Valute[`${validCurrency1}`].Value;
+            validCurrency2 === 'RUB' ? value2 = 1 : value2 = response.data.Valute[`${validCurrency2}`].Value;
+
+                        
+
+        } catch (e) {
+            alert(`Ошибка: ${e.name}. Попробуйте перезагрузить страницу или подключитесь позднее.`);
+        }
+    }
+
+    useEffect(() => {
+        fetchCurrency();
+    })
     
     return (
         <div className='converter'>
