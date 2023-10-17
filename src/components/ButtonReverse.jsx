69d8d6e486c3ef56f38d147686c0d71df18da7a8
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-10-17 11:42:44 +0300

    first working

--- src/components/ButtonReverse.jsx
+++ src/components/ButtonReverse.jsx
@@ -1,10 +1,11 @@
 import { React } from "react";
-// import './css/ButtonReverse.css';
+import imgUrl from '../images/image.png';
+import './css/ButtonReverse.css';
 
 export default function ButtonReverse() {
     return (
         <div id="button__reverse">
-            <img src="../images/image.png" alt="" />
+            <img src={imgUrl} alt="" />
         </div>
     )
 }
