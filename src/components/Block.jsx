c3582b500c6b1715fed6d6b28b8620500e1e68a5
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-10-17 13:09:21 +0300

    delete value in block/input

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -2,11 +2,11 @@ import {React} from 'react';
 import CurrencyPanel from './CurrencyPanel';
 import './css/Block.css';
 
-export default function Block({validCurrency, handleCurrency, value, disabled}) {
+export default function Block({validCurrency, handleCurrency, disabled}) {
     return (
         <div className='block'>
             <CurrencyPanel validCurrency={validCurrency} handleCurrency={handleCurrency}/>
-            <input type="text" value={value} disabled={disabled}/>
+            <input type="text" disabled={disabled}/>
             <div className='info'>RUB = RUB</div>
         </div>
     )
--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -12,13 +12,11 @@ export default function Converter() {
             <Block 
                 validCurrency={validCurrency1}
                 handleCurrency={setValidCurrency1}
-                value={1000}
                 disabled={false}/>
             <ButtonReverse />
             <Block
                 validCurrency={validCurrency2}
                 handleCurrency={setValidCurrency2}
-                value={0}
                 disabled={true}/>
         </div>
     )
